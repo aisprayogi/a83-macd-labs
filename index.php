9aaccf12cfbd367a386d3351b728df3154fd4121
A. Ais Prayogi <10903674+aisprayogi@users.noreply.github.com>
2019-06-25 02:05:14 +0800

    update config server

--- index.php
+++ index.php
@@ -27,14 +27,10 @@
  </form>
  <?php
 
-    try {
-       $conn = new PDO("sqlsrv:server = tcp:macd-db-server.database.windows.net,1433; Database = macd-submit1-db", "aisprayogi", "HariSeninTanggal24");
-       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
+    $connectionInfo = array("UID" => "aisprayogi@macd-db-server", "pwd" => "{your_password_here}", "Database" => "macd-submit1-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:macd-db-server.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
     if (isset($_POST['submit'])) {
         try {
