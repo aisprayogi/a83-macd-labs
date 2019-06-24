f5bbcfbe0c0c3e6b4a5f73d632a65a3fc82912f3
A. Ais Prayogi <10903674+aisprayogi@users.noreply.github.com>
2019-06-25 01:46:14 +0800

    update DB server config

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "macd-db-server.database.windows.net";
+    $user = "aisprayogi";
+    $pass = "HariSeninTanggal24";
+    $db = "macd-submit1-db";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
